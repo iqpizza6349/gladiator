da4acb6b0b6a52361a1b452cf4cd263b21d7fe4f
iqpizza6349 <iqpizza6349@gmail.com>
2025-01-19 16:09:26 +0900

    게임 시간 단위를 Float 에서 Double 로 변경

--- src/main/kotlin/game/GameSpeed.kt
+++ src/main/kotlin/game/GameSpeed.kt
@@ -4,11 +4,11 @@ package io.iqpizza.game
  * 게임 속도가 보통을 기준으로 1분 game-loop 가 1350 임을 가정하여,
  * 초당 22.5 로 계산했습니다.
  */
-enum class GameSpeed(val perFrameCount: Float) {
-    SLOWEST(13.5F),
-    SLOW(18F),
-    NORMAL(22.5F),
-    FAST(27F),
-    FASTEST(31.5F)
+enum class GameSpeed(val perFrameCount: Double) {
+    SLOWEST(13.5),
+    SLOW(18.0),
+    NORMAL(22.5),
+    FAST(27.0),
+    FASTEST(31.5)
     ;
 }
--- src/main/kotlin/game/InGameClock.kt
+++ src/main/kotlin/game/InGameClock.kt
@@ -2,10 +2,10 @@ package game
 
 import kotlin.math.round
 
-data class InGameClock(private val gameLoopsPerSecond: Float) {
+data class InGameClock(private val gameLoopsPerSecond: Double) {
 
     // game-loop 를 기반으로 현재 시간을 초 단위로 계산 (반올림 하도록 한다.)
-    fun calculateSeconds(currentGameLoop: Long): Float {
+    fun calculateSeconds(currentGameLoop: Long): Double {
         return round(currentGameLoop / gameLoopsPerSecond)
     }
 }
