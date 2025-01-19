e949bf69a4d00a13c313c90cc9831930360a1a9a
iqpizza6349 <iqpizza6349@gmail.com>
2025-01-19 20:56:18 +0900

    fetchUser 함수 구현

--- src/main/kotlin/system/GameController.kt
+++ src/main/kotlin/system/GameController.kt
@@ -1,10 +1,15 @@
 package io.iqpizza.system
 
 import com.github.ocraft.s2client.bot.S2Agent
+import com.github.ocraft.s2client.bot.gateway.UnitInPool
+import io.iqpizza.unit.GameUnit
 
 /**
  * SC2 와 직접 소통을 수행하는 컨트롤러
  */
 class GameController(private val agent: S2Agent) {
 
+    fun fetchUnit(unit: GameUnit): UnitInPool {
+        return agent.observation().getUnit(unit.tag)
+    }
 }
