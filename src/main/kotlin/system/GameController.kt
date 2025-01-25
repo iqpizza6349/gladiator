17126343fc2b922230ae0465ed9d4ab55fa0f5c7
iqpizza6349 <iqpizza6349@gmail.com>
2025-01-26 01:36:49 +0900

    유닛 생산 기능 추가

--- src/main/kotlin/system/GameController.kt
+++ src/main/kotlin/system/GameController.kt
@@ -2,6 +2,11 @@ package io.iqpizza.system
 
 import com.github.ocraft.s2client.bot.S2Agent
 import com.github.ocraft.s2client.bot.gateway.UnitInPool
+import com.github.ocraft.s2client.protocol.data.Abilities
+import com.github.ocraft.s2client.protocol.data.Units
+import com.github.ocraft.s2client.protocol.unit.Alliance
+import com.github.ocraft.s2client.protocol.unit.Unit
+import io.iqpizza.unit.Filters
 import io.iqpizza.unit.GameUnit
 
 /**
@@ -11,5 +16,60 @@ class GameController(private val agent: S2Agent) {
 
     fun fetchUnit(unit: GameUnit): UnitInPool {
         return agent.observation().getUnit(unit.tag)
+    }
+
+    fun existsUnitType(units: Collection<Units> = emptySet()): Boolean {
+        return fetchUnit(units).isNotEmpty()
+    }
+
+    // 남은 인구수 조회
+    fun fetchRemainingSupply(): Int {
+        val maxSupply = fetchMaxSupply()    // 인구수 최대 공급량
+        val usedSupply = fetchUsedSupply()   // 사용한 인구수
+        return maxSupply - usedSupply
+    }
+
+    fun fetchMaxSupply(): Int {
+        return agent.observation().foodCap
+    }
+
+    fun fetchUsedSupply(): Int {
+        return agent.observation().foodUsed
+    }
+
+    fun fetchArmySupply(): Int {
+        return agent.observation().foodArmy
+    }
+
+    fun fetchWorkerSupply(): Int {
+        return agent.observation().foodWorkers
+    }
+
+    fun enoughResource(requiredMinerals: Int, requiredVespene: Int): Boolean {
+        val currentMineral = agent.observation().minerals
+        val currentVespene = agent.observation().vespene
+        return currentMineral >= requiredMinerals && currentVespene >= requiredVespene
+    }
+
+    fun trainUnit(issuers: Collection<Units>, desireTo: Abilities) : Boolean {
+        val idleUnit = fetchIdleUnit(issuers) ?: return false
+        agent.actions().unitCommand(idleUnit, desireTo, true)
+        return true
+    }
+
+    fun fetchIdleUnit(unitTypes: Collection<Units>): Unit? {
+        val filter = fetchUnit(unitTypes).filter { Filters.isIdle(it) }
+        val unitInPool = filter.firstOrNull() ?: return null
+        val idleUnit = unitInPool.unit ?: return null
+        return  idleUnit.get()
+    }
+
+    fun fetchUnit(unitTypes: Collection<Units>, alliance: Alliance = Alliance.SELF): List<UnitInPool> {
+        return try {
+            agent.observation().getUnits { Filters.isUnitType(it, unitTypes, alliance) }
+        } catch (e: ConcurrentModificationException) {
+            // Java 와 Kotlin 차이로 인해 발생함
+            emptyList()
+        }
     }
 }
